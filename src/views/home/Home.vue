38a8587c6862f5e2c0cea533bb0ca00a1bb7e339
wgz <a18819545917@163.com>
2022-03-06 11:00:15 +0800

    导航栏组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,14 +1,37 @@
 <template>
   <div id="home" class="wrapper">
-    home
+    <!-- 导航栏 -->
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Home",
-  }
+// 导航栏
+import NavBar from "components/common/navbar/NavBar";
+
+export default {
+  name: "Home",
+  components: {
+    NavBar
+  },
+};
 </script>
 
 <style scoped>
+#home {
+  /*padding-top: 44px;*/
+  height: 100vh;
+  position: relative;
+}
+
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
 </style>
