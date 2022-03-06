1f6ac1b0568b29f6df1d9cd183e75f60ae7ea81e
wgz <a18819545917@163.com>
2022-03-06 09:38:38 +0800

    基础结构搭建

---
+++ src/views/cart/Cart.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="home" class="wrapper">
+    cart
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Cart",
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="home" class="wrapper">
+    profile
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Profile",
+  }
+</script>
+
+<style scoped>
+</style>
